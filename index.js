82619e5dac4f418302544836bcf10c1d87ba6bc3
mohammadrizvy <rizvyhq1@gmail.com>
2023-07-03 00:48:00 +0600

    Vercel config file added

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get("/chef",(req,res) => {
     res.send(chef);
 });
 
+app.get("/chef/:id",(req,res) => {
+    const id = req.params.id;
+    const selectedChef = chef.find(n => n.id === id)
+    res.send(selectedChef);
+
+    console.log(id);
+})
+
 app.listen(600,() => {
     console.log("server is running on port 600")
 });
